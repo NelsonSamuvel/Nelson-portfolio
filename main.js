1270b309e9606219edf297781d0d70d945833e17
NelsonSamuvel <nelsonsamvl@gmail.com>
2025-04-17 21:55:17 +0530

    add outside click function

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const initApp = () => {
   const year = new Date().getFullYear();
   footer.textContent = year;
 
+  const state = { isOpenSidebar: false };
+
   // Header
   const header = document.querySelector("header");
 
@@ -17,26 +19,63 @@ const initApp = () => {
   const ul = document.querySelector(".header__ul");
   const i = hamIcon.lastElementChild;
 
+  // All navLinks
+  const navLists = document.querySelectorAll(".header__ul li a");
+
   hamIcon.addEventListener("click", () =>
-    toggleSidebar(faBars, faXmark, ul, i)
+    toggleSidebar(faBars, faXmark, ul, i, state)
   );
 
   // Scroll
   window.addEventListener("scroll", () => handleScroll(header));
-};
 
-const toggleSidebar = (faBars, faXmark, ul, i) => {
-  i.classList.toggle(faBars);
-  i.classList.toggle(faXmark);
+  navLists.forEach((list) => {
+    list.addEventListener("click", () => {
+      if (state.isOpenSidebar) {
+        toggleSidebar(faBars, faXmark, ul, i, state);
+      }
+    });
+  });
 
-  ul.classList.toggle("header__ul--show");
-  ul.classList.toggle("header__ul--hidden");
-};
+  document.addEventListener("click", (e) => {
+    handleOutsideClick(e, ul, hamIcon, state);
+  });
 
-const handleScroll = (header) => {
-  if (window.scrollY > 50) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+  const toggleSidebar = (faBars, faXmark, ul, i, state) => {
+    i.classList.toggle(faBars);
+    i.classList.toggle(faXmark);
+
+    const iconBtn = document.querySelector(".header__icon");
+
+    if (i.classList.contains("fa-xmark")) {
+      iconBtn.style.position = "fixed";
+      state.isOpenSidebar = true;
+    } else {
+      iconBtn.style.position = "static";
+      state.isOpenSidebar = false;
+    }
+
+    ul.classList.toggle("header__ul--show");
+    ul.classList.toggle("header__ul--hidden");
+  };
+
+  // Scroll functionality
+  const handleScroll = (header) => {
+    if (window.scrollY > 50) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  };
+
+  // Outside Click
+  const handleOutsideClick = (e, ul, hamIcon, state) => {
+    // Only close if sidebar is open
+    if (!state.isOpenSidebar) return;
+
+    // If clicked outside of both the sidebar and the hamburger icon
+    if (!ul.contains(e.target) && !hamIcon.contains(e.target)) {
+      toggleSidebar(faBars, faXmark, ul, i, state);
+    }
+  };
 };
